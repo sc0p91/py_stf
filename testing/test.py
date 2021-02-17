aea3a85ebdf4c5dfc99fd5abf1b7bcc39df7ce25
swan <sc0p.sdc@gmail.com>
2021-02-17 08:51:34 +0100

    cleanup

--- testing/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-this = "Hallo Wäut lulz"
-that = ""
-
-for i in this:
-    that = that + i
-    print(that)
-    
--- testing/test.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-this='Hallo Wäut lulz'
-
-for i in $this; do that=$that$i; echo $that; done 
